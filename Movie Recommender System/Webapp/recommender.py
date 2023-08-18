57c8897b67e546612f1a159a7705faa80b680020
eddyrehman <102432176+eddyrehman@users.noreply.github.com>
2023-08-18 11:27:52 +0530

    Add files via upload

---
+++ Movie Recommender System/Webapp/recommender.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=275a316d10f59d608db84f0486d726cd'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+def recommend(movie):
+     movie_index = movies[movies ['title']==movie].index[0]
+     distances = similarity[movie_index]
+     movies_list = sorted(list (enumerate(distances)),reverse= True, key=lambda x:x[1])[1:6]
+    
+     recommended_movies=[] 
+     recommended_movies_poster=[]
+     for i in movies_list:
+         #fetching movies poster
+          movie_id = movies.iloc[i[0]].movie_id
+          recommended_movies.append(movies.iloc[i[0]].title) 
+          #fetching posterfrom api
+          recommended_movies_poster.append(fetch_poster(movie_id))
+     return recommended_movies,recommended_movies_poster
+
+ 
+movies_dict= pickle.load(open('/Users/adeeburrehman/Desktop/MovieRecommender/Webapp/movies1.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('/Users/adeeburrehman/Desktop/MovieRecommender/Webapp/similarity.pkl','rb'))
+
+
+st.title('Movie Recommendation Sysytem')
+
+
+selected_movie_name = st.selectbox(
+    'Select The Name of The Movie',
+    movies['title'].values
+)
+if st.button('Recommend'):
+    names,posters = recommend(selected_movie_name)
+   
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.header(names[0])
+        st.image(posters[0])
+        
+
+    with col2:
+        st.header(names[1])
+        st.image(posters[1])
+   
+    with col3:
+        st.header(names[2])
+        st.image(posters[2])
+   
+    with col4:
+        st.header(names[3])
+        st.image(posters[3])
+   
+    with col5:
+        st.header(names[4])
+        st.image(posters[4])
+   
